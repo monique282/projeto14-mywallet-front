384c1d24b0e22deae531b934b7d80132c28665f4
monique <moniquejp16@gmail.com>
2023-07-07 20:21:32 -0300

    teste de cadastro e login 1.3

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -39,7 +39,7 @@ export default function SignInPage() {
         <MyWalletLogo />
         <input placeholder="E-mail" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} disabled={disabled} data-test="email"/>
         <input placeholder="Senha" type="password" autoComplete="new-password" required value={senha} onChange={(e) => setSenha(e.target.value)} disabled={disabled} data-test="password"/>
-         <button type='submit' disabled={disabled} data-test="sing-up-submit">
+         <button type='submit' disabled={disabled} data-test="sing-in-submit">
           {disabled ? (
             <ThreeDots width={32} height={21} border-radius={4.5} background-color="#A328D6" color="#FFFFFF" font-size={9} />
           ) : (
